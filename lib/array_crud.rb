d7719447984ea9431093a8a5a85197672db84e3b
Moufoulyroun Kouferidji <micromouf@yahoo.fr>
2019-03-05 12:44:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
+  []
   
 end
 
 def create_an_array
-  
+  ["green", "yellow", "red", "orange"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  [2, 4, 6] << 8
 end
 
 def add_element_to_start_of_array(array, element)
